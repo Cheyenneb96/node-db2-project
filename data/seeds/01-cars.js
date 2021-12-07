07c4a4a9219ab38c8393548a074a95be8d1e0e52
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-12-07 14:06:42 -0600

    up

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
+const { default: knex } = require("knex")
+
 // STRETCH
+const cars =[
+    {
+        vin:'12345678987654321',
+        make: 'toyota',
+        module: 'prius',
+        milage: 2000,
+        title: 'salvage',
+        
+    },
+    {
+        vin:'12345678909876543',
+        make: 'gmc',
+        module: 'seirra',
+        milage: 250000,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin:'98765432123456789',
+        make: 'chevrolet',
+        module: 'trax',
+        milage: 6000,
+        
+    },
+]
+exports.seed= async function(knex){
+   await knex('cars').truncate()
+   await knex('cars').insert(cars)
+    
+}
